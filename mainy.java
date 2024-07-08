c4016544ce3390c9fc33fbe2fb1fa6eebc2fb258
anandyadav38 <yadavanandyadav38@gmail.com>
2024-07-08 13:37:11 +0530

    Print Inverted Half Pyramid.

--- mainy.java
+++ mainy.java
@@ -2,18 +2,13 @@ import java.util.*;
 
 public class mainy {
     public static void main(String[] args) {
-        int n = 5; // Number of rows in the pyramid
+        int n = 4; 
         
         // Outer loop for rows
-        for (int i = 1; i <= n; i++) {
-            // Print spaces for alignment
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            
+        for (int i = n; i >= 1; i--) {
             // Inner loop for numbers
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
+                System.out.print(i + " ");
             }
             
             // Move to the next line after each row
