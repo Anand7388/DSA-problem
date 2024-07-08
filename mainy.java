1fe8bbcdb8e65dc81e77d658535de0d7d0aee02d
anandyadav38 <yadavanandyadav38@gmail.com>
2024-07-08 17:39:15 +0530

    Write a function to multiply 2 numbers.

--- mainy.java
+++ mainy.java
@@ -1,18 +1,22 @@
 import java.util.*;
 
+
 public class mainy {
-    public static void main(String[] args) {
-        int n = 4; 
-        
-        // Outer loop for rows
-        for (int i = n; i >= 1; i--) {
-            // Inner loop for numbers
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
-            }
-            
-            // Move to the next line after each row
-            System.out.println();
-        }
-    }
+
+
+   //Multiply 2 numbers
+   public static int multiply(int a, int b) {
+       return a*b;
+   }
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+
+
+       int result = multiply(a, b);
+       System.out.println(result);
+   }
 }
+
